9751d19f6e98b99e96faba1b4704f80312b398f9
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-07-13 15:09:27 +0530

    updated

--- dist/script.js
+++ dist/script.js
@@ -54,11 +54,11 @@ var swiper = new Swiper(".swiper", {
   centerSlide: "true",
   fade: "true",
   grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  //   dynamicBullets: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
