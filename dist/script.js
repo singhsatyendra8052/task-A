6e533edb1fad867426426bee9c72b05f12c4ae69
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-07-20 16:25:00 +0530

    updated the courses

--- dist/script.js
+++ dist/script.js
@@ -133,3 +133,14 @@ function transformDiv(translateValue, buttonId) {
     }
   });
 }
+
+function toggle(className, idname) {
+  const elements = document.getElementsByClassName(className);
+  const specificElement = document.getElementById(idname);
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add("hidden");
+  }
+
+  specificElement.classList.remove("hidden");
+}
